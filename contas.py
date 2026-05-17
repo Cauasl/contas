8aeef43afe3c0fb3b35ef2c47e48c4c38943638b
Cauã S. Lopes <154275368+Cauasl@users.noreply.github.com>
2026-05-17 18:44:28 -0300

    Função criação de conta

--- contas.py
+++ contas.py
@@ -2,33 +2,7 @@ import sys
 from datetime import datetime
 import os
 import json
-
-def procurarIndexValor(arry=[], val=str):
-    try:
-        if arry.index(val):
-          return True
-    except ValueError:
-        return False
-
-def seComando(comd=str):
-  listaComandos = ['-ad', '-pp', '-pc', '-v', '-status']
-  try:
-    if listaComandos.index(comd) >= 0:
-      return True
-  except ValueError:
-    return False
-
-def verificacaoEntradaConta(arry=[]):
-  comandos = ['-pp', '-pc']
-  try:
-    print(float(arry[1]))
-    for i in range(0, len(comandos)):
-      if len(arry) > 2 and arry[2] == comandos[i]:
-        return []
-      elif i == len(comandos) - 1:
-          return arry
-  except ValueError:
-    return arry
+import funcoes
 
 # nome dos arquivos mês_ano
 # -al //faz a alteraçao no arquivo criado
@@ -55,51 +29,26 @@ if not os.path.isfile(caminho + '/contas.json') and len(args) == 0:
     print('O arquivo não existe ainda, deseja criar outro?')
     res = input('Sim/Não: ')
     if res == 's' or res == 'sim' or res == 'Sim':
-        contaArray = []
-
 
         #Pergunta e já adiciona o sálario
         valorSalario = float(input('Qual o sálario do mês: '))
-        valorLiquido = 0 
-        #Pergunta sobre qual conta quer adicionar e seu valor
-        resConta = []
-        while not procurarIndexValor(resConta, '-t'): #Loop para adicionar quantos valores quiser // -t termina o loop
-          resConta = input('Conta dinheiro -tipo: ').split()
-          if len(resConta) < 2:
-            continue
-          
-          resErroInput = verificacaoEntradaConta(resConta) #Caso não compra os requisitos retorna uma arry vazia
+        
+        sla = funcoes.criacaoConta()
 
-          while len(resErroInput) > 0:
-            fraseInput = " ".join(resErroInput)
-            print('Valor não segue o esperado.')
-            resConta = input(f"Conta dinheiro -tipo: {fraseInput} ").split()
-            if len(resConta) == 0:
-              continue
-            
-            for i, item in enumerate(resErroInput):
-              if i < len(resErroInput)-1: #Coloca os valores anteriores ao index com valor errado
-                resConta.insert(i, item)
-            resErroInput = verificacaoEntradaConta(resConta)
-
-          contaArray.append(resConta)
-          valorLiquido += float(resConta[1])
-
-
-        valorTotal = valorSalario - valorLiquido
+        valorTotal = valorSalario - sla['valorLiquido']
         contaObj = [{
             "valorSalario": valorSalario,
-            "valorLiquido": valorLiquido,
-            "valorLiquidoTotal": valorLiquido
+            "valorLiquido": sla['valorLiquido'],
+            "valorLiquidoTotal": sla['valorLiquido']
         }]
         #Cria o caminho e adiciona os respectivos valores ao arquivo
         if not os.path.exists(caminho) or not os.path.exists(caminho+'/contas.json'):
             os.makedirs(caminho)
-            for i in range(0, len(contaArray)):
+            for i in range(0, len(sla['objConta'])):
                 contaObj.append({
-                    "conta": contaArray[i][0],
-                    "valor": float(contaArray[i][1]),
-                    "comando": contaArray[i][2],
+                    "conta": sla['objConta'][i][0],
+                    "valor": float(sla['objConta'][i][1]),
+                    "comando": sla['objConta'][i][2],
                     "status": 'nao_paga'
                 })
             contaJson = json.dumps(contaObj, ensure_ascii=False, indent=4)
@@ -174,7 +123,7 @@ for a, argumento in enumerate(args): #Torna possível colocar mais argumentos
       #Adiciona uma conta para o arquivo do mês            
       case '-ad':
         # -ad contaParaAdicionar valor comando
-        if seComando(argumento):
+        if funcoes.seComando(argumento):
           json_carregado[0]['valorLiquidoTotal'] = json_carregado[0]['valorLiquidoTotal'] + float(args[i+2])
           json_carregado.append({
                       "conta": args[a+1],
@@ -186,4 +135,5 @@ for a, argumento in enumerate(args): #Torna possível colocar mais argumentos
             arq.write(json.dumps(json_carregado, ensure_ascii=False, indent=4))
             
       case '-s': #simulação
-        criacaoConta()
+        simu = funcoes.criacaoConta()
+        print(simu['valorLiquido'])
--- funcoes.py
+++ funcoes.py
@@ -1,5 +1,34 @@
+def procurarIndexValor(arry=[], val=str):
+    try:
+        if arry.index(val):
+          return True
+    except ValueError:
+        return False
+
+def seComando(comd=str):
+  listaComandos = ['-ad', '-pp', '-pc', '-v', '-status']
+  try:
+    if listaComandos.index(comd) >= 0:
+      return True
+  except ValueError:
+    return False
+
+def verificacaoEntradaConta(arry=[]):
+  comandos = ['-pp', '-pc']
+  try:
+    print(float(arry[1]))
+    for i in range(0, len(comandos)):
+      if len(arry) > 2 and arry[2] == comandos[i]:
+        return []
+      elif i == len(comandos) - 1:
+          return arry
+  except ValueError:
+    return arry
+
 def criacaoConta():
   dadosConta = []
+  objConta = []
+  valorLiquido = 0
   while not procurarIndexValor(dadosConta, '-t'):
     dadosConta = input('Conta dinheiro -tipo: ').split()
     if len(dadosConta) < 2:
@@ -12,10 +41,22 @@ def criacaoConta():
       print('Valor não segue o esperado.')
       fraseInput = " ".join(contaIncompleta)
       dadosConta = input(f"Conta dinheiro -tipo: {fraseInput} ").split()
-      if len(resConta) == 0:
+      if len(dadosConta) == 0:
         continue
 
       for i, item in enumerate(contaIncompleta):
         if i < len(contaIncompleta)-1: #Coloca os valores anteriores ao index com valor errado
-        dadosConta.insert(i, item)
-        contaIncompleta = verificacaoEntradaConta(dadosConta)
+          dadosConta.insert(i, item)
+          contaIncompleta = verificacaoEntradaConta(dadosConta)
+
+    valorLiquido += float(dadosConta[1])
+    objConta.append(dadosConta)
+
+  if len(objConta) > 0:
+    return {
+      "objConta": objConta,
+      "valorLiquido": valorLiquido
+    }
+  else:
+    print("ERRO: Vazio")
+    return {}
